8d4e910ee335edbc83ad6aed33f668ca456b66a8
Anthony Muo <a.c.muo.16@aberdeen.ac.uk>
2021-04-30 15:48:29 +0100

    1st Commit

---
+++ testexample.py
@@ -0,0 +1,80 @@
+from tkinter import *
+from tkinter import filedialog
+import tkinter as tk
+import pytesseract
+from PIL import Image, ImageTk
+import os
+
+
+def readTxt1():
+    fln = filedialog.askopenfilename(
+        initialdir=os.getcwd(),
+        title="select Image File",
+        filetype=(("JPG file", "*.jpg"), ("PNG file", "*.png"), ("All Files", "*.*")),
+    )
+    t1.set(fln)
+
+
+# creat showimage function
+def showimage():
+    fln = filedialog.askopenfilename(
+        initialdir=os.getcwd(),
+        title="select Image File",
+        filetype=(("JPG file", "*.jpg"), ("PNG file", "*.png"), ("All Files", "*.*")),
+    )
+    img = Image.open(fln)
+    img.thumbnail((350, 350))
+    img = ImageTk.PhotoImage(img)
+    # to show the image
+    lbl.configure(image=img)
+    lbl.image = img
+
+    #  Get OCR output using Pytesseract
+    custom_config = r"--oem 3 --psm 6"
+    txt3.delete("1.0", "end")
+    # txt2 = pytesseract.image_to_string(Image.open(fln), config=custom_config)
+    txt3.insert(
+        INSERT, pytesseract.image_to_string(Image.open(fln), config=custom_config)
+    )
+
+
+root = Tk()
+
+t1 = StringVar()
+wrapper = LabelFrame(root, text="Choose File")
+wrapper.pack(fill="both", expand="yes", padx=10, pady=10)
+
+wrapper2 = LabelFrame(root, text="Receipt Image ")
+wrapper2.pack(fill="both", expand="yes", padx=10, pady=10)
+
+
+lbl = Label(root)
+lbl.pack()
+
+wrapper3 = LabelFrame(root, text="Image Text")
+wrapper3.pack(fill="both", expand="yes", padx=10, pady=10)
+
+txt = Entry(wrapper, textvariable=t1)
+txt.pack(side=tk.LEFT, padx=10, pady=10)
+
+btn = Button(wrapper, text="Browse", command=readTxt1)
+btn.pack(side=tk.LEFT, padx=10, pady=10)
+
+btn2 = Button(wrapper, text="Exit", command=lambda: exit())
+btn2.pack(side=tk.LEFT, padx=10, pady=10)
+
+btn3 = Button(wrapper, text="Browse Image", command=showimage)
+btn3.pack(side=tk.LEFT, padx=10, pady=10)
+
+
+# txt2 = Text(wrapper2)
+# txt2.pack(padx=10, pady=10)
+
+txt3 = Text(wrapper3)
+txt3.pack(padx=10, pady=10)
+
+
+root.geometry("900x650")
+# root.title("text reader")
+root.resizable(False, False)
+root.mainloop()
